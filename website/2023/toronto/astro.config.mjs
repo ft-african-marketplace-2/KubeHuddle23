3a019db3bffa37d36b409a9c35320efb6f67123a
saudkhanbpk <saudkhanbpk@gmail.com>
2023-02-02 01:10:32 +0500

    fixed all the conflicts and did all the new udpates

--- website/2023/toronto/astro.config.mjs
+++ website/2023/toronto/astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/2023/toronto/",
+  // base: "/2023/toronto/",
+  base: "",
   vite: {
     ssr: {
       external: ["svgo"],
